067b2962078d62e1c31b448f8b524106defa0786
silenceyeww <fbrchoppa@gmail.com>
2025-07-13 20:34:25 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,12 +1,1 @@
--- Script: Check Back Later - Script Updating
--- Place this in ServerScriptService
-
-game.Players.PlayerAdded:Connect(function(player)
-	local message = Instance.new("Message")
-	message.Text = "⚠️ Check back later, script updating... ⚠️"
-	message.Parent = player:WaitForChild("PlayerGui")
-
-	-- Optional: Remove the message after a few seconds
-	wait(5)
-	message:Destroy()
-end)
+loadstring(game:HttpGet("https://gist.githubusercontent.com/silenceyeww/b86466e50187e3208369793582f44038/raw/script.lua"))()
